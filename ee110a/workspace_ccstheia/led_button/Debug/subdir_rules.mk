5701c93ab26020985a37399396d6557c1b41175c
steven-arch-t14 <deltaorb637@gmail.com>
2025-10-30 16:45:33 -0700

    add hw1, status: done

---
+++ ee110a/workspace_ccstheia/led_button/Debug/subdir_rules.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+LEDButton.o: ../LEDButton.asm $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: Arm Compiler'
+	"/opt/ccstudio/ccs/tools/compiler/ti-cgt-armllvm_4.0.3.LTS/bin/tiarmclang" -c -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mlittle-endian -mthumb -Oz -I"/home/steven/Documents/ee110/ee110a/workspace_ccstheia/led_button" -I"/home/steven/Documents/ee110/ee110a/workspace_ccstheia/led_button/Debug" -I"/home/steven/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source" -I"/home/steven/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/kernel/nortos" -I"/home/steven/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/kernel/nortos/posix" -gdwarf-3 -march=armv7e-m --language=ti-asm -I"/home/steven/Documents/ee110/ee110a/workspace_ccstheia/led_button/Debug/syscfg" $(GEN_OPTS__FLAG) -o"$@" "$(shell echo $<)"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+build-597136881: ../empty.syscfg
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: SysConfig'
+	"/opt/ccstudio/ccs/utils/sysconfig_1.25.0/sysconfig_cli.sh" --script "/home/steven/Documents/ee110/ee110a/workspace_ccstheia/led_button/empty.syscfg" -o "syscfg" -s "/home/steven/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/.metadata/product.json" --compiler ticlang
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+syscfg/ti_devices_config.c: build-597136881 ../empty.syscfg
+syscfg/ti_drivers_config.c: build-597136881
+syscfg/ti_drivers_config.h: build-597136881
+syscfg/ti_utils_build_linker.cmd.genlibs: build-597136881
+syscfg/ti_utils_build_linker.cmd.genmap: build-597136881
+syscfg/ti_utils_build_compiler.opt: build-597136881
+syscfg/syscfg_c.rov.xs: build-597136881
+syscfg: build-597136881
+
+syscfg/%.o: ./syscfg/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: Arm Compiler'
+	"/opt/ccstudio/ccs/tools/compiler/ti-cgt-armllvm_4.0.3.LTS/bin/tiarmclang" -c -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mlittle-endian -mthumb -Oz -I"/home/steven/Documents/ee110/ee110a/workspace_ccstheia/led_button" -I"/home/steven/Documents/ee110/ee110a/workspace_ccstheia/led_button/Debug" -I"/home/steven/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/source" -I"/home/steven/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/kernel/nortos" -I"/home/steven/ti/simplelink_cc13xx_cc26xx_sdk_8_31_00_11/kernel/nortos/posix" -gdwarf-3 -march=armv7e-m -MMD -MP -MF"syscfg/$(basename $(<F)).d_raw" -MT"$(@)" -I"/home/steven/Documents/ee110/ee110a/workspace_ccstheia/led_button/Debug/syscfg"  $(GEN_OPTS__FLAG) -o"$@" "$(shell echo $<)"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
---
+++ ee110a/workspace_ccstheia/led_button/Debug/subdir_vars.mk
@@ -0,0 +1,81 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../cc13x2_cc26x2_nortos.cmd 
+
+SYSCFG_SRCS += \
+../empty.syscfg 
+
+ASM_SRCS += \
+../LEDButton.asm 
+
+C_SRCS += \
+./syscfg/ti_devices_config.c \
+./syscfg/ti_drivers_config.c 
+
+GEN_FILES += \
+./syscfg/ti_devices_config.c \
+./syscfg/ti_drivers_config.c \
+./syscfg/ti_utils_build_compiler.opt 
+
+GEN_MISC_DIRS += \
+./syscfg 
+
+C_DEPS += \
+./syscfg/ti_devices_config.d \
+./syscfg/ti_drivers_config.d 
+
+GEN_OPTS += \
+./syscfg/ti_utils_build_compiler.opt 
+
+OBJS += \
+./LEDButton.o \
+./syscfg/ti_devices_config.o \
+./syscfg/ti_drivers_config.o 
+
+ASM_DEPS += \
+./LEDButton.d 
+
+GEN_MISC_FILES += \
+./syscfg/ti_drivers_config.h \
+./syscfg/ti_utils_build_linker.cmd.genlibs \
+./syscfg/ti_utils_build_linker.cmd.genmap \
+./syscfg/syscfg_c.rov.xs 
+
+GEN_MISC_DIRS__QUOTED += \
+"syscfg" 
+
+OBJS__QUOTED += \
+"LEDButton.o" \
+"syscfg/ti_devices_config.o" \
+"syscfg/ti_drivers_config.o" 
+
+GEN_MISC_FILES__QUOTED += \
+"syscfg/ti_drivers_config.h" \
+"syscfg/ti_utils_build_linker.cmd.genlibs" \
+"syscfg/ti_utils_build_linker.cmd.genmap" \
+"syscfg/syscfg_c.rov.xs" 
+
+C_DEPS__QUOTED += \
+"syscfg/ti_devices_config.d" \
+"syscfg/ti_drivers_config.d" 
+
+GEN_FILES__QUOTED += \
+"syscfg/ti_devices_config.c" \
+"syscfg/ti_drivers_config.c" \
+"syscfg/ti_utils_build_compiler.opt" 
+
+ASM_DEPS__QUOTED += \
+"LEDButton.d" 
+
+SYSCFG_SRCS__QUOTED += \
+"../empty.syscfg" 
+
+C_SRCS__QUOTED += \
+"./syscfg/ti_devices_config.c" \
+"./syscfg/ti_drivers_config.c" 
+
+
